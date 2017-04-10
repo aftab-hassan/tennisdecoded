bd658f7666a0fccf0f898ca757bca4a15c2b011a
Aftab Hassan <aftab.hassan@ge.com>
2017-04-10 08:03:55 -0700

    cutting out top 10 and bottom 10

--- src/main/java/scripts/boxplot.R
+++ src/main/java/scripts/boxplot.R
@@ -18,10 +18,6 @@ jpeg("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/secondServ
 boxplot(df$secondServePointsWonPercentage, ylab='Percentage', main='Second serve points won percentage')
 dev.off()
 
-jpeg("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/firstServePointsWonPercentage.jpeg")
-boxplot(df$firstServePointsWonPercentage, ylab='Percentage', main='First serve points won percentage')
-dev.off()
-
 jpeg("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/serviceGamesWonPercentage.jpeg")
 boxplot(df$serviceGamesWonPercentage, ylab='Percentage', main='Service games percentage')
 dev.off()
@@ -33,3 +29,32 @@ dev.off()
 jpeg("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/averageDoubleFaultsPerMatch.jpeg")
 boxplot(df$averageDoubleFaultsPerMatch, ylab='Percentage', main='Average double faults per match')
 dev.off()
+
+#summary
+setwd("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/")
+df = read.csv("servestats.csv")
+
+summary(df$firstServePercentage)
+summary(df$firstServePointsWonPercentage)
+summary(df$secondServePointsWonPercentage)
+summary(df$serviceGamesWonPercentage)
+summary(df$averageAcesPerMatch)
+summary(df$averageDoubleFaultsPerMatch)
+
+#median cutting
+setwd("/Users/aftabhassan/Documents/workspaceneon/TennisDecoded/output/")
+df = read.csv("servestats.csv")
+factors = c("firstServePercentage","firstServePointsWonPercentage","secondServePointsWonPercentage","serviceGamesWonPercentage","averageAcesPerMatch","averageDoubleFaultsPerMatch")
+for(f in factors){
+	print(f)
+	print(summary(df[,f])["Median"])
+	print(summary(df[,f][5:70])["Median"])
+	print(summary(df[,f][10:65])["Median"])
+	print(summary(df[,f][15:60])["Median"])
+	print(summary(df[,f][20:55])["Median"])
+	print(summary(df[,f][25:50])["Median"])
+	print(summary(df[,f][30:45])["Median"])
+	print(summary(df[,f][35:40])["Median"])
+	print("done!")
+	cat(paste("\n"));
+}
